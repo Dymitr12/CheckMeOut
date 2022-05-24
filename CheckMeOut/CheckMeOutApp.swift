bba4a22b3e8f3ae469b281beff56c6ccade9810d
Dymitr12 <dk39fra@gmail.com>
2022-05-24 23:53:59 +0300

    Initial Commit

---
+++ CheckMeOut/CheckMeOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMeOutApp.swift
+//  CheckMeOut
+//
+//  Created by Dymitr on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckMeOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
